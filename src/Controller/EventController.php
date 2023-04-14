83c0611b56a837bac0108aeb76ce38c350d3091a
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-04-14 21:44:04 +0200

    finished

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/event')]
+#[Route('/')]
 class EventController extends AbstractController
 {
     #[Route('/', name: 'app_event_index', methods: ['GET'])]
